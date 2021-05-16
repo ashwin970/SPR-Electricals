7557f4b01b16e30ce6f2da1f57344fcd15c226f9
ashwin970 <ashwinraji97@gmail.com>
2021-05-16 20:13:00 +0530

    enter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends React.Component{
         return(
            <>
            <nav>
-                <div className="nav-wrapper indigo">
+                <div className="nav-wrapper">
                 <div className="NavChild1">   
                 <a href="#!" class="brand-logo">
                     <img src={logo} className="logo"/>
@@ -17,10 +17,10 @@ class Navbar extends React.Component{
                 <div className="NavChild2">
                 <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                 <ul className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="/products">Products</a></li>
+                    <li><a href="#">Enquiry</a></li>
+                    {/* <li><a href="mobile.html">Mobile</a></li> */}
                 </ul>
                 </div>
                 </div>

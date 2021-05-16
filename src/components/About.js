9170595615b735e0486c37cffd8ed76a01bfcb5c
ashwin970 <ashwinraji97@gmail.com>
2021-05-16 16:48:56 +0530

    enter

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import img from '../media/img4.jpg';
+import cl1 from '../media/cardimages/card-logo1.png';
+import cl2 from '../media/cardimages/card-logo2.png';
+import pi1 from '../media/pimg1.jpg';
+import 'jquery-parallax.js';
 
 class About extends React.Component{
     render(){
         return(
             <>
+            <div className="keeper">
                 <div className="main">
                     <section className="section-left">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla cum unde perferendis libero. Quae, dicta cum eos, voluptatum sunt enim ab consectetur, mollitia molestiae atque non cupiditate hic odio.
@@ -14,6 +19,36 @@ class About extends React.Component{
                         <img src={img} className="section-right-img"/>
                     </section>
                 </div>
+
+                <div class="parallax-window" data-parallax="scroll"
+                data-z-index="1" data-image-src={pi1}>
+                        
+                </div>
+
+                
+
+                <div className="container">
+                    <div className="card">
+                        <div className="imgBx">
+                            <img src={cl1} className="cl1"/>
+                        </div>
+                        <div className="content">
+                            <h5>Best Service</h5>
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla cum unde perferendis libero. Quae, dicta cum eos, voluptatum sunt enim ab consectetur, mollitia molestiae atque non cupiditate hic odio.</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="imgBx">
+                            <img src={cl2} className="cl1"/>
+                        </div>
+                        <div className="content">
+                            <h5>Good Products</h5>
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla cum unde perferendis libero. Quae, dicta cum eos, voluptatum sunt enim ab consectetur, mollitia molestiae atque non cupiditate hic odio.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </>
         );
     }
